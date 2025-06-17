7df5317c54cb47d402056c4e234d6aa14323ab5f
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-06-17 20:13:40 +0200

    feat: add header main and footer sections to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
+import header from './header.js';
+import main from './main_screen.js';
+import footer from './footer.js';
 
 const mainBody = document.body;
 
 function createWrapper() {
-	const wrapper = document.createElement("div");
-	wrapper.className = "wrapper";
+	const wrapper = document.createElement('div');
+	wrapper.className = 'wrapper';
+	return wrapper;
+}
+
+function createMainPage() {
+	const wrapper = createWrapper();
+	wrapper.appendChild(header);
+	wrapper.appendChild(main);
+	wrapper.appendChild(footer);
 	mainBody.insertAdjacentElement('afterbegin', wrapper);
 }
 
 mainBody.onload = () => {
-	createWrapper();
-}
+	createMainPage();
+};
