7591e9793d251c0f97e021eaee3292952d3d803f
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-06-17 20:33:40 +0200

    refactor: add text to footer in footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,7 +4,11 @@ class Footer {
 
 	#createFooter() {
 		const footer = document.createElement('footer');
+		const smallText = document.createElement('small');
 		footer.classList.add('footer');
+		smallText.classList.add('footer__small-text');
+		smallText.textContent = 'Copyright © 2025 Thread Calculator. All Rights Reserved.'
+		footer.appendChild(smallText);
 		return footer;
 	}
 
