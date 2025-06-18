edee6269c3c7ce3c1ef28fe9383b96f49707e3eb
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-06-18 21:42:09 +0200

    refactor: move wrapper functions to wrapper.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,3 @@
-import header from './header.js';
-import main from './main_screen.js';
-import footer from './footer.js';
+import Wrapper from './wrapper.js';
 
-const mainBody = document.body;
-
-function createWrapper() {
-	const wrapper = document.createElement('div');
-	wrapper.className = 'wrapper';
-	return wrapper;
-}
-
-function createMainPage() {
-	const wrapper = createWrapper();
-	wrapper.appendChild(header);
-	wrapper.appendChild(main);
-	wrapper.appendChild(footer);
-	mainBody.insertAdjacentElement('afterbegin', wrapper);
-}
-
-mainBody.onload = () => {
-	createMainPage();
-};
+new Wrapper();
