1a924a6b2fe899733182c4aa319c01d0bd207d20
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-06-23 21:04:54 +0200

    refactor: add ButtonsContainer import to MainSection class

--- scripts/main_screen.js
+++ scripts/main_screen.js
@@ -1,14 +1,16 @@
 import PointsContainer from './points_container.js';
+import ButtonsContainer from './buttons_block.js';
 
 class MainSection {
 	constructor() {
 		this.pointContainer = new PointsContainer().getPointsContainer();
+		this.buttonsContainer = new ButtonsContainer().getButtonsContainer();
 	}
 
 	#createMainSection() {
 		const mainSection = document.createElement('main');
 		mainSection.classList.add('main');
-		mainSection.appendChild(this.pointContainer);
+		mainSection.append(this.pointContainer, this.buttonsContainer);
 		return mainSection;
 	}
 
