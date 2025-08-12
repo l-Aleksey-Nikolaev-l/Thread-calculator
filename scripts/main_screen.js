2d55dde6d7ae11fa1c6e39dc7979a4b360df2f95
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-08-12 16:47:22 +0200

    feat: add output field to main section

--- scripts/main_screen.js
+++ scripts/main_screen.js
@@ -1,16 +1,28 @@
 import PointsContainer from './points_container.js';
 import ButtonsContainer from './buttons_block.js';
+import Output from './output_field.js';
 
 class MainSection {
 	constructor() {
 		this.pointContainer = new PointsContainer().getPointsContainer();
 		this.buttonsContainer = new ButtonsContainer().getButtonsContainer();
+		this.outputArea = new Output().getOutputArea();
+	}
+
+	#createSection(sectionName) {
+		const newSection = document.createElement('section');
+		newSection.classList.add(sectionName);
+		return newSection;
 	}
 
 	#createMainSection() {
 		const mainSection = document.createElement('main');
 		mainSection.classList.add('main');
-		mainSection.append(this.pointContainer, this.buttonsContainer);
+		const inputSection = this.#createSection('input_section');
+		const outputSection = this.#createSection('output_section');
+		inputSection.append(this.pointContainer, this.buttonsContainer);
+		outputSection.append(this.outputArea);
+		mainSection.append(inputSection, outputSection);
 		return mainSection;
 	}
 
