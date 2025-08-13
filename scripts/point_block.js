694f2ed9d5d6f3166ecb9b601dab71c8a456f180
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-08-13 11:26:40 +0200

    refactor: add function for creating fields and add field for X point

--- scripts/point_block.js
+++ scripts/point_block.js
@@ -3,19 +3,30 @@ class PointBlock {
 		this.blockNumber = 1;
 	}
 
-	#createBlockContainer() {
-		const blockContainer = document.createElement('div');
+	#addPointField(fieldName) {
+		const pointField = document.createElement('div');
 		const pointLabel = document.createElement('label');
 		const pointValue = document.createElement('input');
-		blockContainer.classList.add('point_block');
 		pointLabel.classList.add('point_label');
-		pointLabel.setAttribute('for', `point_${this.blockNumber}`);
-		pointValue.classList.add('point_value');
-		pointValue.id = `point_${this.blockNumber}`;
-		pointLabel.textContent = `Point ${this.blockNumber} = Z`
+		pointLabel.setAttribute('for', `${fieldName}_point_${this.blockNumber}`);
+		pointValue.classList.add('point_value', `point_value_${fieldName}`);
+		pointValue.id = `${fieldName}_point_${this.blockNumber}`;
+		pointLabel.textContent = `${fieldName}`
 		pointValue.value = '';
-		pointValue.placeholder = '0'
-		blockContainer.append(pointLabel, pointValue);
+		pointValue.placeholder = '0 mm'
+		pointField.append(pointLabel, pointValue);
+		return pointField;
+	}
+
+	#createBlockContainer() {
+		const blockContainer = document.createElement('div');
+		const blockLabel = document.createElement('p');
+		const pointX = this.#addPointField('X');
+		const pointZ = this.#addPointField('Z');
+		blockContainer.classList.add('point_block');
+		blockLabel.classList.add('block_label');
+		blockLabel.textContent = `Point ${this.blockNumber}:`
+		blockContainer.append(blockLabel, pointX, pointZ);
 		this.blockNumber += 1;
 		return blockContainer;
 	}
