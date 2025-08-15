372fcc544c7e144d32c118bff4b3f7ca2987b9a0
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-08-15 15:11:40 +0200

    refactor: add separate condition for disable constant pitch field

--- scripts/switch_variable_pitch.js
+++ scripts/switch_variable_pitch.js
@@ -2,15 +2,17 @@ const variablePitchInputs = document.getElementsByClassName('variable_pitch');
 const constantPitchInputs = document.getElementsByClassName('common_pitch_container');
 
 function switchVariablePitch(isChecked) {
-
+	if (isChecked) {
+		constantPitchInputs[0].classList.add('disable_element');
+	} else {
+		constantPitchInputs[0].classList.remove('disable_element');
+	}
 	for (const variablePitchInput of variablePitchInputs) {
 		if (isChecked) {
 			variablePitchInput.classList.remove('variable_pitch_disabled');
-			constantPitchInputs[0].classList.add('constant_pitch_disabled');
 		}
 		else {
 			variablePitchInput.classList.add('variable_pitch_disabled');
-			constantPitchInputs[0].classList.remove('constant_pitch_disabled');
 		}
 	}
 }
