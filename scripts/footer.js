7d21105a1384a9e16ab7f2077d2c1fa8c42a97e9
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-08-15 16:10:05 +0200

    refactor: add information to footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,11 +4,18 @@ class Footer {
 
 	#createFooter() {
 		const footer = document.createElement('footer');
-		const smallText = document.createElement('small');
+		const copyrightText = document.createElement('small');
+		const authorText = document.createElement('a');
 		footer.classList.add('footer');
-		smallText.classList.add('footer__small-text');
-		smallText.textContent = 'Copyright © 2025 Thread Calculator. All Rights Reserved.'
-		footer.appendChild(smallText);
+		authorText.classList.add('footer_author');
+		const aText = '44§65§73§69§67§6e§65§64§20§61§6e§64§20§64§65§76§65§6c§6f§70§65§64§20§62§79§20§41§6c§65§6b§73§65§79§20§4e§69§6b§6f§6c§61§65§76';
+		const lText = '68§74§74§70§73§3a§2f§2f§77§77§77§2e§6c§69§6e§6b§65§64§69§6e§2e§63§6f§6d§2f§69§6e§2f§6e§69§6b§6f§6c§61§65§76§61§6c§65§6b§73§65§79§2f';
+		const rText = '43§6f§70§79§72§69§67§68§74§20§a9§20§32§30§32§35§2e§20§41§6c§6c§20§52§69§67§68§74§73§20§52§65§73§65§72§76§65§64';
+		copyrightText.textContent = this.#dec(rText);
+		authorText.textContent = this.#dec(aText);
+		authorText.href = this.#dec(lText);
+		authorText.target = '_blank';
+		footer.append(copyrightText, authorText);
 		return footer;
 	}
 
